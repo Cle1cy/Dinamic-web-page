9d41357167689a8ae264391fef32079bd9fe1f1d
Cle1cy <psydiego1@gmail.com>
2023-02-20 18:03:51 -0500

    Almacenamiento info del usuario local sotrage

--- adminkit-3.1.0/static/js/registro.js
+++ adminkit-3.1.0/static/js/registro.js
@@ -1,1 +1,29 @@
-    
+let impNombre = document.getElementById('nombre');
+let impEmpresa = document.getElementById('compañia');
+let impCorreo = document.getElementById('correo');
+let impContraseña = document.getElementById('contraseña');
+let impRegistrarse = document.getElementById('formSingUp');  
+
+
+impRegistrarse.addEventListener('submit', valDatos);
+
+function valDatos(){
+
+    const Usuario = 
+    {
+        nombre:impNombre.value,
+        contraseña:impContraseña.value,
+        correo:impCorreo.value,
+        empresa:impEmpresa.value
+    }
+
+    localStorage.setItem('Usuario',JSON.stringify(Usuario));
+
+}
+
+console.log(JSON.parse(localStorage.getItem('Usuario')));
+console.log(impNombre.value);
+console.log(impEmpresa.value);
+console.log(impCorreo.value);
+console.log(impContraseña.value);
+
