26f369a037d286c52063d8af57f8ec06a6fc2b15
Cle1cy <psydiego1@gmail.com>
2023-02-20 19:14:17 -0500

    Registro con validacion

--- adminkit-3.1.0/static/js/registro.js
+++ adminkit-3.1.0/static/js/registro.js
@@ -7,28 +7,49 @@ let impRegistrarse = document.getElementById('formSingUp');
 
 impRegistrarse.addEventListener('submit', valDatos);
 
-function valDatos(){
+function valDatos(event)
+{
+    let valdiacionCorreo =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    let valdiacionContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}[^'\s]/;
 
-    
-    if(!impNombre.value || !impContraseña.value || impCorreo.value || impEmpresa.value){
-        alert("rellenar todos los campos");
-    }
-
-    const Usuario = 
+    if(impNombre.value.length < 6 || impNombre.value.length > 60 )
     {
-        nombre:impNombre.value,
-        contraseña:impContraseña.value,
-        correo:impCorreo.value,
-        empresa:impEmpresa.value
+        event.preventDefault();
+        alert("El nombre debe ser mayor a 6 caracters");
+        
+    }  
+    else if(impEmpresa.value.length < 3 || impEmpresa.value.length > 60 )
+    {
+        event.preventDefault();
+        alert("El nombre de la empresa debe ser mayor a 6 caracters");
     }
-
-    localStorage.setItem('Usuario',JSON.stringify(Usuario));
-
+    else if(!valdiacionCorreo.test(impCorreo.value))
+    {
+        event.preventDefault();
+        alert("El correo electronico no es valido");
+    }
+    else if(!valdiacionContraseña.test(impContraseña.value))
+    {
+        event.preventDefault();
+        alert("La contraseña debe contener 1 caracter especial, un numero, una mayuscula" +
+                 "No ser menor a 8 caracteres o mayor a 15 caracteres");
+    }
+    else
+    {
+        const Usuario = 
+        { 
+            nombre:impNombre.value,
+            contraseña:impContraseña.value,
+            correo:impCorreo.value,
+            empresa:impEmpresa.value
+        }
+        
+        localStorage.setItem('Usuario',JSON.stringify(Usuario));
+    }
 }
-
+array.forEach(element => {
+    
+});
 console.log(JSON.parse(localStorage.getItem('Usuario')));
-console.log(impNombre.value);
-console.log(impEmpresa.value);
-console.log(impCorreo.value);
-console.log(impContraseña.value);
+
 
