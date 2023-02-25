b0ecb879458ce323fe44ac702089cdc9cd939930
Cle1cy <psydiego1@gmail.com>
2023-02-25 18:00:29 -0500

    Labels al no colcoar datos bien login, signup

--- adminkit-3.1.0/static/js/registro.js
+++ adminkit-3.1.0/static/js/registro.js
@@ -9,8 +9,11 @@ let impRegistrarse = document.getElementById('formSignUp');
 let impContraseñaSingIn = document.getElementById('contraseñaSignIn');
 let impCorreoSingIn = document.getElementById('correoSignIn');
 let impLogearse = document.getElementById('formSignIn');
-//Enlaces externos-------------------------------------------------------->
-
+//ERRORES-------------------------------------------------------->
+let errorNombre = document.getElementById('form-label-error-nombre');
+let errorCorreo = document.getElementById('form-label-error-correo');
+let erroresCompañia = document.getElementById('form-label-error-compañia');
+let erroresContraseña = document.getElementById('form-label-error-contraseña');
 
 //----------------------------------------------------------------------->
 if(impLogearse != null) impLogearse.addEventListener('submit', Login);
@@ -19,32 +22,35 @@ if(impRegistrarse != null) impRegistrarse.addEventListener('submit', valDatos);
 
 //Funciones ------------------------------------------------------------->
 
+
 function valDatos(event)
 {
     let valdiacionCorreo =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     let valdiacionContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}[^'\s]/;
-
+    
     if(impNombre.value.length < 6 || impNombre.value.length > 60 )
     {
         event.preventDefault();
-        alert("El nombre debe ser mayor a 6 caracters");
-        
+        errorNombre.classList.remove('hiden');
     }  
     else if(impEmpresa.value.length < 3 || impEmpresa.value.length > 60 )
     {
         event.preventDefault();
-        alert("El nombre de la empresa debe ser mayor a 6 caracters");
+        errorNombre.classList.add('hiden');
+        erroresCompañia.classList.remove('hiden');
     }
     else if(!valdiacionCorreo.test(impCorreo.value))
     {
         event.preventDefault();
-        alert("El correo electronico no es valido");
+        erroresCompañia.classList.add('hiden');
+        errorCorreo.classList.remove('hiden');
     }
     else if(!valdiacionContraseña.test(impContraseña.value))
     {
         event.preventDefault();
-        alert("La contraseña debe contener 1 caracter especial, un numero, una mayuscula" +
-                 "No ser menor a 8 caracteres o mayor a 15 caracteres");
+        errorCorreo.classList.add('hiden');
+        erroresContraseña.classList.remove('hiden');
+       
     }
     else
     {
@@ -60,19 +66,21 @@ function valDatos(event)
     }
 }
 
-function Login(event){
 
+function Login(event){
+let error = document.getElementById('form-label-error');
+let good = document.getElementById('form-label-good');
 verificarUsuario = JSON.parse(localStorage.getItem('Usuario'));
 
 if(verificarUsuario.correo !== impCorreoSingIn.value || 
     verificarUsuario.contraseña !== impContraseñaSingIn.value)
 {
     event.preventDefault();
-    alert("Usuario o contraseña no validos");
-    console.log('ErrorLogin');
+    error.classList.remove('hiden')
 }
 else
 {
+    error.classList.add('hiden')
     let secionUsuario = 
     {
         contraseña:impContraseñaSingIn.value,
@@ -80,12 +88,14 @@ else
     }
 
     localStorage.setItem('secionUsuario',JSON.stringify(secionUsuario));
+
 } 
+
 
 }
 
 
-/*
+
 console.log(JSON.parse(localStorage.getItem('Usuario')));
 console.log(JSON.parse(localStorage.getItem('secionUsuario')));
 verificarUsuario = JSON.parse(localStorage.getItem('Usuario'));
@@ -93,4 +103,4 @@ console.log(verificarUsuario.correo);
 console.log(impCorreoSingIn.value);
 console.log(verificarUsuario.contraseña);
 console.log(impContraseñaSingIn.value );
-*/
+
