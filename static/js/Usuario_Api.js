1c525e881a9155b8be661ef799814108d00b6d23
= <=>
2023-03-09 20:57:17 -0500

    Primera versión API

--- static/js/Usuario_Api.js
+++ static/js/Usuario_Api.js
@@ -1,18 +1,17 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/users';
+const API_URL = 'http://jasonplaceholder.typicode.com';
 
 const xhr = new XMLHttpRequest();
 
-function onRequestHandler() {
+function onRequestHandler(){
 	if(this.readyState === 4 && this.status === 200){
-
 		const data = JSON.parse(this.response);
-		const HTMLResponse = document.querySelector("#UserApi");
+		const HTMLResponse = document.querySelector("#app");
 
-		const tpl = data.map(user => `<li>👤 ${user.name} 📧 ${user.email}</li>`);
-		HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
+		const tpl = data.map((user) => '<li>${user.name} 📧 ${user.email}</li>');
+		HTMLResponse.innerHTML = '<ul>${tpl}</ul>';
 	}
 }
 
 xhr.addEventListener("load", onRequestHandler);
-xhr.open('GET', `${API_URL}/users`);
+xhr.open('GET', '${API_URL}/users');
 xhr.send();
